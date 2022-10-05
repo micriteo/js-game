c85dc2b0e3a74a68ab39d16c97c283fe8109e457
micriteo <76212872+micriteo@users.noreply.github.com>
2022-10-05 11:45:13 +0200

    1st chapter

---
+++ js/snowpack.config.js
@@ -0,0 +1,23 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: "es2018",
+  },
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
